8a65faf1f4e174061dbd423d664800c285b454e0
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-08-09 17:24:55 -0500

    Add bonus join Exercise.

---
+++ join_bonus.sql
@@ -0,0 +1,17 @@
+-- Write a query to get the average salary current managers make by gender 
+-- M 79350.60
+-- F 75690.00
+
+
+-- Write a query to get the avg salary managers historically make by gender
+-- M 72810.94
+-- F 62037.21
+
+
+-- get avg salary for all current empoyees by gender
+-- M 72044.66
+-- F 71963.57
+
+-- get avg historic salary for all employees by gender
+-- M 63842.25
+-- F 63750.17
